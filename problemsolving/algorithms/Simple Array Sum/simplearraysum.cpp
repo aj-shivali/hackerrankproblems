883782b332ddcc67aa243b7b516a775798ef2cde
Shiwali <45665313+aj-shivali@users.noreply.github.com>
2021-05-13 20:18:56 +0530

    Update simplearraysum.cpp

--- problemsolving/algorithms/Simple Array Sum/simplearraysum.cpp
+++ problemsolving/algorithms/Simple Array Sum/simplearraysum.cpp
@@ -1,1 +1,14 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+int main(){
+    int n,result=0;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+        result=result+arr[i];
+    }
+    cout<<result;
+}
