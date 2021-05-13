a06a20a7966a267fa84b65b6b1901a59ab3051c5
Shiwali <45665313+aj-shivali@users.noreply.github.com>
2021-05-14 01:44:30 +0530

    Create Compare the triplets.cpp

---
+++ problemsolving/algorithms/Compare The Triplets/Compare the triplets.cpp
@@ -0,0 +1,22 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+  int a[3], p = 0, q = 0;
+  scanf("%d %d %d", &a[0], &a[1], &a[2]);
+  int b[3];
+  scanf("%d %d %d", &b[0], &b[1], &b[2]);
+  for (int i = 0; i < 3; i++) {
+    if (a[i] > b[i])
+      p++;
+    else if (b[i] > a[i])
+      q++;
+  }
+  printf("%d %d", p, q);
+  return 0;
+}
