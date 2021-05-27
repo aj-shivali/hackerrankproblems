74faf35467fb7c5dedc59ece591644e4524e9654
Shiwali <45665313+aj-shivali@users.noreply.github.com>
2021-05-28 02:12:23 +0530

    Update Plus Minus.cpp

--- problemsolving/algorithms/Plus Minus/Plus Minus.cpp
+++ problemsolving/algorithms/Plus Minus/Plus Minus.cpp
@@ -1,1 +1,27 @@
-
+#include<bits/stdc++.h>
+#include<iostream>
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+using namespace std;
+int main() {
+  int n;
+  float pos = 0, neg = 0, zer = 0;
+  cin >> n;
+  int arr[n];
+  for (int arr_i = 0; arr_i < n; arr_i++) {
+    cin >> arr[arr_i];
+    if (arr[arr_i] > 0)
+      pos++;
+    else if (arr[arr_i] < 0)
+      neg++;
+    else
+      zer++;
+  }
+  cout<<(float)pos / n << endl << (float)neg / n << endl << (float)zer / n << endl;
+  return 0;
+}
